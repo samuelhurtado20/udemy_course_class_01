253e052c0c6ab29a7417f8146521c1e4228c84a4
samuelhurtado20 <samuelhurtado20@hotmail.com>
2022-05-15 21:41:46 -0500

    refactor: register-vehicle

--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -2,18 +2,16 @@ import { HttpRequest, HttpResponse } from '../interfaces/http-interface'
 
 export class RegisterVehicle {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('name is missing')
+    const requiredProperties = ['name','model','year']
+    for (const pro of requiredProperties) {
+      if (!httpRequest.body[pro]) {
+        return {
+          statusCode: 400,
+          body: new Error(`${pro} is missing`)
+        }
       }
     }
-    if (!httpRequest.body.model) {
-      return {
-        statusCode: 400,
-        body: new Error('model is missing')
-      }
-    }
+
     return {
       statusCode: 200,
       body: 'Everytring is okey'
